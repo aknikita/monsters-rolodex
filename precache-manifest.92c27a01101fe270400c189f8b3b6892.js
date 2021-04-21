d6c306c1774aed4876d7a74e89b0fb58b2205934
Nikita Akimenko <nikitaakimenko@Nikitas-MacBook-Pro.local>
2021-04-21 10:23:21 +0200

    Updates

---
+++ precache-manifest.92c27a01101fe270400c189f8b3b6892.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3e4192ed5588620b583fc72a185207a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "44259d3457da8d0cf897",
+    "url": "/monsters-rolodex/static/css/main.baf43ead.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44259d3457da8d0cf897",
+    "url": "/monsters-rolodex/static/js/main.383f3d8f.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
